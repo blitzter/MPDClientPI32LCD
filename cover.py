0e2e6bc8690575335de154d48cdcb454b228868b
blitzter <blitzter@gmail.com>
2016-09-11 14:45:53 +0530

    removed unnecessary file

--- cover.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python3
-import subprocess, os
-process = subprocess.Popen("./coverart.sh", shell=True, stdout=subprocess.PIPE).stdout.read()
-#print "Content-type: image/png\n"
-fileName, fileExtension = os.path.splitext(process)
-print("Content-Type: image/" + fileExtension[1:].lower() + "\n")
-with open(process, 'rb') as f:
-	print(f.read())
